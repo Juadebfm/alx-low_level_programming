a5599d1e38c13905d2d66b763d0fffab4df6958e
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-13 16:27:01 -0700

    The Main Thing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
+* main - prints _putchar followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	_putchar('M');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('i');
-	_putchar('n');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
