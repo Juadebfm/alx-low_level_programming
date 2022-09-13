fcf49bbfc622bd4aa60ec518c6410ebea0c29a86
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-13 15:37:35 -0700

    Real Deal

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
+#include "juadeb.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('J');
+	_putchar('u');
+	_putchar('a');
+	_putchar('d');
+	_putchar('e');
+	_putchar('b');
+	_putchar('\n');
 
+	return (0);
+}
