bbd85bd7fb76bc67fda57908b81dabe597914383
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-14 05:48:56 -0700

    error

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -main function
+ * no Description
+ * Return:0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
