677eb6392df1e2c91f53863d5c9a938599016462
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-16 11:14:15 -0700

    fix

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
-
-
-
 /**
-
-  * main - Prints the alphabet at reverse
-
-  *
-
-  * Return: Always (Success)
-
-  */
-
+ * main - Prints the alphabet at reverse
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 
 {
