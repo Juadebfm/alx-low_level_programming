aa3023f408d7544e7714c9fd7d77eeeb2f5caf04
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-16 09:44:44 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* Description: main - Prints out a string to stdout.
-* Return: 0 if success.
-*/
-
+ * main - Prints "with proper grammar, but the outcome is a piece of art
+ * ," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
