c3561933811ed2c4948d02a545436e041139a04b
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-22 04:01:53 -0700

    Fixes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,6 +4,7 @@
 /**
  *cap_string - this is awesome
  * @s: pointer to char params
+ *
  * Return: *s
  */
 
@@ -19,7 +20,7 @@ char *cap_string(char *s)
 				s[0] = s[0] - 32;
 					for (j = 0; delimeters[j] != '\0'; j++)
 						if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-						s[i + 1] = s[i + 1] - 32;
+					                s[i + 1] = s[i + 1] - 32;
 		}
 		return (s);
 }
