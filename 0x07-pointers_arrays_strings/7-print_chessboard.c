1e6dd3eedb17ca01fc03e469e95b2d43496ff638
Julius Adebowale <juadebgabriel@gmail.com>
2022-09-26 13:06:19 -0700

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,14 +3,14 @@
 /**
  * print_chessboard - Print the chessboard
  * @a: array of pieces
- *
- * Return: nothing.
+*
+ * Return: Nothing.
  */
 void print_chessboard(char (*a)[8])
 {
 	int b, c;
 
-	for (b = 0; b < 8; c++)
+	for (b = 0; b < 8; b++)
 	{
 		for (c = 0; c < 8; c++)
 		{
