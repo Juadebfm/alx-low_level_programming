fc8c2a3860148e6a88418eb9318fdc447a5441ed
Julius Adebowale <juadebgabriel@gmail.com>
2022-10-02 04:10:42 -0700

    fixes

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - print name 
- * @argc: int.
- * @argv: char.
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+*
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 0 ; i < argc ; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
