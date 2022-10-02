dc03980391e87c62b9ca307f765482318f296b42
Julius Adebowale <juadebgabriel@gmail.com>
2022-10-02 04:20:57 -0700

    fixes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,13 +15,14 @@ int main(int argc, char *argv[])
 	int k;
 	int mult = 0;
 	
+	
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (0);
 	}
 	else if (argc == 3)
-	{	
+		{	
 		for (i = 0 ; i < argc ; i++)
 		{
 			if (i == 1)
