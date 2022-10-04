4baed336948926c118b05ff2504f64b5c2ffd0bc
Julius Adebowale <juadebgabriel@gmail.com>
2022-10-04 02:02:10 -0700

    fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,8 +24,7 @@ char *argstostr(int ac, char **av)
 	cat = (char *) malloc(sizeof(char) * size);
 
 	if (!cat)
-		return (NULL);
-	
+		return (NULL);	
 	for (arg = 0, size = 0; arg < ac; ++arg, ++size)
 	{
 		for (chr = av[arg]; *chr; ++chr, ++size)
