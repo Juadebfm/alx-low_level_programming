64170f7f6213b51d0e1930a946c280d667bc0778
Julius Adebowale <juadebgabriel@gmail.com>
2022-10-19 08:36:21 -0700

    fiux

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
  * add - adds two integers 
  * @a: first integers
  * @b: second integer
- *
- * Return: addition of a and b 
+* Return: addition of a and b 
  */
-
 int add(int a, int b)
 {
 	return (a + b);
